370b9fc9e5fff5aa136ec0a94d9a40612fc3bd97
Kento Suzuki <38824465+Goose211@users.noreply.github.com>
2019-09-29 01:48:59 +0900

    Add files via upload

--- lfs6musicsns/models.rb
+++ lfs6musicsns/models.rb
@@ -1,3 +1,8 @@
-
+require 'bundler/setup'
+Bundler.require
 
  ActiveRecord::Base.establish_connection(ENV['DATABASE_URL']||"sqlite3:db/development.db")
+
+ class User < ActiveRecord::Base
+  has_secure_password
+ end
