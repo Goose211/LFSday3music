3e4e18c773d1574566bd5b86c8e0ad7acaafac7d
Kento Suzuki <38824465+Goose211@users.noreply.github.com>
2019-09-29 15:40:43 +0900

    Add files via upload
    投稿してhomeに表示

---
+++ lfs6musicsns/lfs6musicsns/models.rb
@@ -0,0 +1,13 @@
+require 'bundler/setup'
+Bundler.require
+
+ ActiveRecord::Base.establish_connection(ENV['DATABASE_URL']||"sqlite3:db/development.db")
+
+ class User < ActiveRecord::Base
+  has_secure_password
+  has_many :posts
+ end
+
+ class Post < ActiveRecord::Base
+  belongs_to :user
+ end
